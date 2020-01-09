0442683af8c2c8336d7fb19d0f5ff261882ebf81
tamr9678 <tamr9678@gmail.com>
2020-01-09 19:13:56 +0900

    add_validation_to_user_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :encrypted_password, 
+    format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i }
 end
