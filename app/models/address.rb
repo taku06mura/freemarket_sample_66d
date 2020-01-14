cf7913bcb1b67a5112cefc2b2acc7a43dc1d9ae1
tamr9678 <tamr9678@gmail.com>
2020-01-14 14:12:34 +0900

    add_validation_to_models

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,9 @@
 class Address < ApplicationRecord
+
+  validates :postal_code,             presence: true, length: {maximum: 10}, format: {with: /\A[0-9]{7}\z/}
+  validates :prefecture,              presence: true, length: {maximum: 30}
+  validates :city,                    presence: true, length: {maximum: 30}
+  validates :address,                 presence: true, length: {maximum: 100}
+
   belongs_to :user, optional: true
 end
--- app/models/personal_datum.rb
+++ app/models/personal_datum.rb
@@ -1,5 +1,15 @@
 class PersonalDatum < ApplicationRecord
   validates :first_name, :last_name, :kana_first_name, :kana_last_name, 
             :birthday_year, :birthday_month, :birthday_day, :phone_number, presence: true
+  
+  validates :first_name,            presence: true, length: {maximum: 20}
+  validates :last_name,             presence: true, length: {maximum: 20}
+  validates :kana_first_name,       presence: true, length: {maximum: 20}
+  validates :kana_last_name,        presence: true, length: {maximum: 20}
+  validates :birthday_year,         presence: true
+  validates :birthday_month,        presence: true
+  validates :birthday_day,          presence: true
+  validates :phone_number,          length: {maximum: 20}
+  
   belongs_to :user, optional: true
 end
