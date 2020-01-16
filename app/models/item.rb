b14dffc837b84a97b2eb13b65fed17948c708962
watabouyosida <58410391+watabouyosida@users.noreply.github.com>
2020-01-16 12:01:06 +0900

    commit2
    カテゴリ、ブランド、画像以外のフォームを作成

---
+++ app/models/item.rb
@@ -0,0 +1,8 @@
+class Item < ApplicationRecord
+  has_many :images
+  accepts_nested_attributes_for :images
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to :category
+ 
+end
---
+++ db/migrate/20200115083112_create_items.rb
@@ -0,0 +1,19 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.integer :user_id, null: false, foreign_key: true
+      t.string :name, null: false
+      t.string :item_discription, null: false
+      t.integer :category_id, null: false, foreign_key: true
+      t.string :size, null: false
+      t.string :brand_name
+      t.string :quality, null: false
+      t.integer :price, null: false
+      t.string :carriage_fee, null: false
+      t.string :prefecture_name, null: false
+      t.string :delivery, null: false
+      t.string :days, null: false
+      t.timestamps
+    end
+  end
+end
