aac4544c7ecdf410cf2515ec8f6ad2bf92c7ee17
ponyuta <58424732+ponyuta@users.noreply.github.com>
2020-01-20 14:30:52 +0900

    credit-card-registration

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,7 @@ Rails.application.routes.draw do
   end
   resources :purchase, only:[:index] do
   end
+  resources :cards, only:[:index, :new] do
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
