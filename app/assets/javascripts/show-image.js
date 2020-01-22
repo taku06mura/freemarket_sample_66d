2ef92789d7dbe76010c2b376173223e81b708d40
tamr9678 <tamr9678@gmail.com>
2020-01-22 12:37:15 +0900

    fix-detail

--- app/assets/javascripts/show-image.js
+++ app/assets/javascripts/show-image.js
@@ -1,7 +1,5 @@
 $(function(){
-  // 「imageList」内の「img」をマウスオーバーした場合
   $('#is__photo__place__dots img').hover(function(){
-      // マウスオーバーしている画像をメインの画像に反映
       $('.is__photo__place__sample__stage__in__img img').attr('src', $(this).attr('src'));
   });
 });
