587d17af2401feafcc379b33d3bee21ef5797d14
taku06mura <t.lux.g1106@icloud.com>
2020-01-22 16:25:00 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_052604) do
+ActiveRecord::Schema.define(version: 2020_01_21_120136) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture", default: 0, null: false
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2020_01_17_052604) do
     t.datetime "updated_at", null: false
     t.index ["municipality"], name: "index_addresses_on_municipality"
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
