96ddf0fbe61debaff26784428ab5e809beca4129
taku06mura <t.lux.g1106@icloud.com>
2020-01-24 17:13:11 +0900

    payjp導入
    items_controller,
    card_model 編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,16 @@ class ItemsController < ApplicationController
 
   end
 
+  def pay
+    @item = Item.find(params[:id])
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    charge = Payjp::Charge.create(
+    amount: @item.price,
+    card: params['payjp-token'],
+    currency: 'jpy'
+    )
+  end
+
   def get_category_children
     @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
   end
