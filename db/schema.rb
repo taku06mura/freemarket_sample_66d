f54639034c7bef28aeb64d5b9efa998eb77e7ca8
taku06mura <t.lux.g1106@icloud.com>
2020-01-24 12:30:41 +0900

    cardテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_01_15_085947) do
-
+ActiveRecord::Schema.define(version: 2020_01_23_164958) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture", default: 0, null: false
@@ -25,6 +23,14 @@ ActiveRecord::Schema.define(version: 2020_01_15_085947) do
     t.datetime "updated_at", null: false
     t.index ["municipality"], name: "index_addresses_on_municipality"
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -82,6 +88,15 @@ ActiveRecord::Schema.define(version: 2020_01_15_085947) do
     t.index ["user_id"], name: "index_personal_data_on_user_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -97,4 +112,5 @@ ActiveRecord::Schema.define(version: 2020_01_15_085947) do
   end
 
   add_foreign_key "images", "items"
+  add_foreign_key "sns_credentials", "users"
 end
