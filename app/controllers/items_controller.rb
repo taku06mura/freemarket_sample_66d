f56e8be542f5da6b6aa39cc6ce6d144090cbdac0
ponyuta <58424732+ponyuta@users.noreply.github.com>
2020-01-29 16:03:43 +0900

    index_fix

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :set_parent, only: [:new, :create, :edit]
   before_action :set_item, only: [:edit, :update, :destroy]
   def index
-    @items = Item.all
+    @items = Item.all.order("created_at DESC").limit(15)
   end
 
   def show
